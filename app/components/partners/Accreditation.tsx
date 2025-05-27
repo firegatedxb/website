bedd6f8c21b3f24ad88105880d95468743b204ec
jishnum007 <jishnu@globalsurf.ae>
2025-05-27 11:18:52 +0530

    abt

--- app/components/partners/Accreditation.tsx
+++ app/components/partners/Accreditation.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Swiper as SwiperClass } from "swiper";
 import { Autoplay, Grid } from 'swiper/modules';
@@ -10,13 +10,36 @@ import { Partners } from '@/public/types/Common';
 
 
 const Accreditation = ({ data }: { data: Partners }) => {
-    const swiperRef = useRef<SwiperClass | null>(null);
+
+
+  const [showPagination, setShowPagination] = useState(false);
+  const swiperRef = useRef<SwiperClass | null>(null);
+   const [activeIndex, setActiveIndex] = useState(0);
+  useEffect(() => {
+    const swiper = swiperRef.current;
+    if (!swiper) return;
+
+    const updatePaginationVisibility = () => {
+      const totalSlides = swiper.slides.length;
+      const visibleSlides = swiper.params.slidesPerView;
+
+      const gridRows = swiper.params.grid?.rows || 1;
+      const visibleCount = (typeof visibleSlides === 'number' ? visibleSlides : 1) * gridRows;
+
+      setShowPagination(totalSlides > visibleCount);
+    };
+
+    updatePaginationVisibility();
+
+    swiper.on('resize', updatePaginationVisibility);
+    return () => swiper.off('resize', updatePaginationVisibility);
+  }, []);
   return (
     <div className='container'>
         <div className='py-[50px] md:py-[70px] lg:py-[100px] '>
             <div className='flex flex-col  '>
                 <h2 className='text-50 text-site-blue font-medium mb-3 lg:mb-[30px] uppercase'>Accreditation</h2>
-                <p className='text-19 text-black'>Our Products & Systems comply below Certifications, Codes and Authority Requirements</p>
+                <p className='text-19 text-gray'>Our Products & Systems comply below Certifications, Codes and Authority Requirements</p>
             </div>
 
               <div className='py-[30px] md:py-[50px] lg:py-[70px] '>
@@ -69,6 +92,25 @@ const Accreditation = ({ data }: { data: Partners }) => {
     </SwiperSlide>
   ))}
 </Swiper>
+ {showPagination && (
+  <div className="w-full">
+    <div className="container">
+      <div className="flex gap-2 justify-center mt-4 md:mt-[42px]">
+        {data.accredit.map((_, index) => (
+          <button
+            key={index}
+            className={`w-[50px] h-[3px] cursor-pointer rounded-full transition-all duration-300 ${
+              activeIndex === index
+                ? "bg-primary scale-125 max-w-[27px]"
+                : "bg-gray max-w-[9px]"
+            }`}
+            onClick={() => swiperRef.current?.slideToLoop(index)}
+          />
+        ))}
+      </div>
+    </div>
+  </div>
+)}
                </div>
         </div>
     </div>
