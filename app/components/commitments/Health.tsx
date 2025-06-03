76be302520d6c2c4568d1d9762b0b0c051ce73e2
Nived <nived@globalsurf.ae>
2025-06-03 12:39:58 +0530

    fix:build

--- app/components/commitments/Health.tsx
+++ app/components/commitments/Health.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useRef } from 'react'
-import Image from 'next/image'
+// import Image from 'next/image'
 
 import { Commitments } from '@/public/types/Common';
 import { motion, useInView } from 'framer-motion';
